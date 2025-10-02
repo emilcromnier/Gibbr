e8ff77d78b7f6d8f95eab389b0076a595cfcdc98
Pontus <pontus@filen.se>
2025-10-02 14:44:48 +0200

    Set up ReactRouter and skeleton for Navbar View and Presenter

---
+++ client/src/ReactRoot.jsx
@@ -0,0 +1,38 @@
+
+import { observer } from 'mobx-react-lite';
+/*import AppPresenter from './appPresenter';*/
+
+import NavbarPresenter from "./presenters/NavbarPresenter.jsx"
+import {  createHashRouter,  RouterProvider, useParams} from "react-router-dom";
+
+
+
+export default observer(    
+    function ReactRoot(props){
+
+        return (
+                
+               
+                <div>
+                <RouterProvider router={makeRouter()}/>
+                {}
+                
+                </div>
+               
+                );
+        }  
+    
+     )
+
+
+     function makeRouter(){
+        return createHashRouter([
+            {
+                path: "/",
+                element: <div className="general"><NavbarPresenter/> </div>
+            },
+
+
+            
+        ])
+    }
