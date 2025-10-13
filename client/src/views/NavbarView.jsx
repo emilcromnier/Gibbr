dc896aabe98c6b1cd79e009ea7fa9f6e6a31ffe1
emilcromnier <emil.cromnier@gmail.com>
2025-10-13 22:05:29 +0200

    Basic CSS for navbar with implemented BEM naming and structure

--- client/src/views/NavbarView.jsx
+++ client/src/views/NavbarView.jsx
@@ -1,6 +1,8 @@
 import '/src/App.css'
+import '/src/styles/navbar.css'
 import { observer } from 'mobx-react-lite';
 import logo from '../assets/GibbR_new_logo.png.png';
+import { Search } from "lucide-react";
 
 
 
@@ -27,15 +29,23 @@ function Navbar(props) {
     
 
   return(
-  
-    <div className= "navbar_items">
+    
+    <nav className = "navbar">
+        <div className = "navbar__left">
+            <img className ="navbar__logo" src={logo} alt="Gibbr Logo" />
+        </div>
 
-        <img className="navbar_img" src={logo} alt="Gibbr Logo" />
-        <button className="nav_home_btn" onClick={BackToMenuACB} >Home</button>
-        <button className="nav_friends_btn" onClick={ToFriendsACB}>Friends</button>
-        <button className="nav_profile_btn" onClick={ToProfileACB}>Profile</button>        
-        <button className='login_navbar' onClick={ToAuthACB} >Login</button>
-    </div>
+        <div className = "navbar__center">
+            <button className="navbar__button navbar__button--home" onClick={BackToMenuACB} >Home</button>
+            <button className="navbar__button navbar__button--friends" onClick={ToFriendsACB}>Friends</button>
+            <button className="navbar__button navbar__button--profile" onClick={ToProfileACB}>Profile</button>        
+            <button className='navbar__button navbar__button--login' onClick={ToAuthACB} >Login</button>
+        </div>
+
+        <div classname = "navbar__right">
+            <Search className = "navbar__icon"/> 
+        </div>
+    </nav>
 
 
   )
