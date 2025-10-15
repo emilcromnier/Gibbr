7fd83c7df565c256f98173322091e63cb3c1f0fd
janssonm <janssonmatilda01@gmail.com>
2025-10-15 23:52:56 +0200

    Responsive flex layout done

--- client/src/views/GameGridView.jsx
+++ client/src/views/GameGridView.jsx
@@ -1,30 +1,72 @@
 import "/src/App.css";
 import "/src/styles/GameGrid.css";
+import { useState, useEffect } from "react";
 
 function GameGrid(props) {
   const games = props.games;
-  console.log("Games to render:", games);
+  const [maxGames, setMaxGames] = useState(window.innerWidth <= 950 ? 3 : 6);
+
+  useEffect(() => {
+    function handleResize() {
+      setMaxGames(window.innerWidth <= 950 ? 3 : 6);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   function InspectGameACB(gameId) {
     window.location.hash = `#/game/${gameId}`;
   }
 
   return (
-    <div className="game-grid">
-      {games.map((game) => (
-        <div
-          className="game-grid__container"
-          onClick={() => InspectGameACB(game.id)}
-        >
-          <p>{game.title}</p>
-          <img
-            className="game-grid__container-img"
-            key={game.id}
-            src={game.image}
-            alt={game.title}
-          />
+    <div>
+      <div className="hero">
+        {games.slice(0, 1).map((game) => (
+          <div
+            className="recommended-game"
+            onClick={() => InspectGameACB(game.id)}
+          >
+            <img
+              className="recommended-game__img"
+              key={game.id}
+              src={game.image}
+              alt={game.title}
+            />
+          </div>
+        ))}
+        <div className="shortcuts">
+          {games.slice(0, 4).map((game) => (
+          <div
+            className="shortcuts__container"
+            onClick={() => InspectGameACB(game.id)}
+          >
+            <img
+              className="shortcuts__container-img"
+              key={game.id}
+              src={game.image}
+              alt={game.title}
+            />
+          </div>
+        ))}
         </div>
-      ))}
+      </div>
+
+      <div className="carousel">
+        {games.slice(1, 1 + maxGames).map((game) => (
+          <div
+            className="carousel__game"
+            onClick={() => InspectGameACB(game.id)}
+          >
+            <img
+              className="carousel__game-img"
+              key={game.id}
+              src={game.image}
+              alt={game.title}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
