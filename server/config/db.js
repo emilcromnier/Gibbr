9571fae9b7eb75ba4682ee880994e69ec35f0bd8
emilcromnier <emil.cromnier@gmail.com>
2025-10-15 10:41:14 +0200

    Fixed mongo cloud

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
+// Uses MONGO Atlas URI from compose file
+
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connected");
+    console.log("MongoDB connected successfully!");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
     process.exit(1);
